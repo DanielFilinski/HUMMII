904786e0d38aec10b0eee4ddede18dc0f93adfee
FilinSky <daniel.filinski@gmail.com>
2025-10-30 18:07:07 +0300

    docker compose build success

---
+++ api/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    '**/*.(t|j)s',
+  ],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapping: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^@core/(.*)$': '<rootDir>/core/$1',
+    '^@shared/(.*)$': '<rootDir>/shared/$1',
+    '^@config/(.*)$': '<rootDir>/config/$1',
+  },
+};
